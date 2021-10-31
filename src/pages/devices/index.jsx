e17431ab1b2904b01b30953e3d3ba9e90c5a753a
Artur Krol <kroolar@gmail.com>
2021-10-31 18:09:01 +0100

    Add rooms and devices table

---
+++ src/pages/devices/index.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { useQuery } from '../../query'
+import {
+  Alert,
+  Box,
+  Icon,
+  Table
+} from '../../components'
+
+const Devices = () => {
+  const params = new URLSearchParams(window.location.search).get('success')
+
+  const { data:devices, remove } = useQuery('devices')
+  const { data:rooms } = useQuery('rooms')
+  
+  const tableData = devices.map(({
+    icon,
+    id,
+    gpio,
+    name,
+    room_id,
+    state
+  }) => {
+    const room = rooms.find(({ id }) => id == room_id)
+
+    return (
+      [
+        <Icon type={icon} />,
+        gpio,
+        name,
+        room?.name,
+        <div className={`device__state device__state--${state}`} />,
+        <Icon onClick={() => remove(id)} type="trash" />
+      ]
+    )
+  })
+
+  return (
+    <div>
+      <Box title="Devices" action="/devices/new">
+        <Table
+          data={tableData}
+          headers={['Icon', 'GPIO', 'Name', 'Room', 'State']}
+        />
+      </Box>
+
+      <Alert text={params} />
+    </div>
+  )
+}
+
+export default Devices
---
+++ src/pages/rooms/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useQuery } from '../../query'
+import {
+  Alert,
+  Box,
+  Icon,
+  Table
+} from '../../components'
+
+const Rooms = () => {
+  const { data:rooms, remove, loading } = useQuery('rooms')
+  const params = new URLSearchParams(window.location.search).get('success')
+
+  const tableData = rooms.map(({ devices, id, name }) => {
+    const icon = (
+      <Icon
+        className="text-gray-800"
+        onClick={() => remove(id)}
+        type="trash"
+      />
+    )
+
+    return [name, devices.length, icon]
+  })
+
+  return (
+    <div>
+      <Box title="Rooms" action="/rooms/new">
+        <Table
+          data={tableData}
+          headers={['Name', 'Devices', '']}
+        />
+      </Box>
+
+      <Alert text={params} />
+    </div>
+  )
+}
+
+export default Rooms
